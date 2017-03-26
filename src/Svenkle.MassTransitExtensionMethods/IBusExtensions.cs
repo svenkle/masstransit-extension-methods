266f1d0715f64f70075f85d83d60e8de66615c8d
Luke Arentz <luke.arentz@dt.com.au>
2017-03-27 10:48:59 +1100

    Added CreateRequestClient extension for URI

--- src/Svenkle.MassTransitExtensionMethods/IBusExtensions.cs
+++ src/Svenkle.MassTransitExtensionMethods/IBusExtensions.cs
@@ -11,5 +11,10 @@ namespace Svenkle.MassTransitExtensionMethods
         {
             return bus.GetSendEndpoint(new Uri($"{bus.Address.GetBaseUri()}/{queue}"));
         }
+
+        public static IRequestClient<TRequest, TResponse> CreateRequestClient<TRequest, TResponse>(this IBus bus, string queue, TimeSpan timeout, TimeSpan? ttl = null, Action<SendContext<TRequest>> callback = null) where TRequest : class where TResponse : class
+        {
+            return bus.CreateRequestClient<TRequest, TResponse>(new Uri($"{bus.Address.GetBaseUri()}/{queue}"), timeout, ttl, callback);
+        }
     }
 }
