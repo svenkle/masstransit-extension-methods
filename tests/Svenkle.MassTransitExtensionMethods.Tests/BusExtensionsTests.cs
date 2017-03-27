d6cb5b9eabaf2bfa2c31431e0359db748d18ca6e
Luke Arentz <luke.arentz@dt.com.au>
2017-03-27 11:12:17 +1100

    Created Unit Test project for Bus Types

---
+++ tests/Svenkle.MassTransitExtensionMethods.Tests/BusExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Xunit;
+
+namespace Svenkle.MassTransitExtensionMethods.Tests
+{
+    public class BusExtensionsTests
+    {
+        public class TheGetSendEndpointMethod : BusExtensionsTests
+        {
+            [Fact]
+            public void ReturnsNullIfBusIsNull()
+            {
+                // Prepare & Act
+                var endpoint = ((IBus)null).GetSendEndpoint("q");
+
+                // Assert
+                Assert.Null(endpoint);
+            }
+        }
+    }
+}
